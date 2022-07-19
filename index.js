73d483859432a1fa51559bd966268370d43e95de
Smart System <smartsystememail@gmail.com>
2022-07-19 21:48:13 +0430

    Modify pre commit

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 console.log('Hello Code Format');
 
-for (let i = 0; i < 10 ; i++) {
-
-
-        console.log('This is test');
-    
+for (let i = 0; i < 10; i++) {
+	console.log('This is test');
 }
